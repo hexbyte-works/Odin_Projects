ce272a5d823f961f9db5d9b4cd8b46e8d1260796
hexbyte-work <peteti.ruthvik@gmail.com>
2026-08-29 18:56:07 +0530

    added some elements in html file and did some ui changes in css file, and and workin gon the logc in js file

---
+++ Rock_Paper_Scissors/game.js
@@ -0,0 +1,66 @@
+function machineChoice(){
+    let num = Math.random()
+    if(num<1/3){
+        return "rock"
+    }
+    else if(num<2/3){
+        return "paper"
+    }
+    else{
+        return "scissor"
+    }
+}
+function tellVictor(hc,mc){
+    const victor= document.getElementById('victor')
+    if(hc==mc){
+            victor.innerText="Victor: None"
+        }
+        else if(hc=="rock" && mc=="scissor"){
+            victor.innerText="Victor: Human"
+            humanScore++
+            document.getElementById('human-score').innerText=`Human Score: ${humanScore}`
+        }
+        else if(hc=="paper" && mc=="rock"){
+            victor.innerText="Victor: Human"
+            humanScore++
+            document.getElementById('human-score').innerText=`Human Score: ${humanScore}`
+        }
+        else if(hc=="scissor" && mc=="paper"){
+            victor.innerText="Victor: Human"
+            humanScore++
+            document.getElementById('human-score').innerText=`Human Score: ${humanScore}`
+        }
+        else{
+            victor.innerText="Victor: Computer"
+            computerScore++
+            document.getElementById('computer-score').innerText=`Computer Score: ${computerScore}`
+        }
+}
+function showMove(hm,cm){
+    const humanMoveText= document.getElementById('human-move')
+    humanMoveText.innerText=`Your Move: ${hm}`
+    const compMoveText= document.getElementById('game-move')
+    compMoveText.innerText=`Computer's Move: ${cm}`
+}
+
+let humanScore=0
+let computerScore=0
+const rockMove = document.getElementById('rock-button')
+const paperMove = document.getElementById('paper-button')
+const scissorMove = document.getElementById('scissor-button')
+
+rockMove.addEventListener('click',()=>{
+    const compMove = machineChoice()
+    showMove('rock',compMove)
+    tellVictor('rock',compMove)
+})
+paperMove.addEventListener('click',()=>{
+    const compMove = machineChoice()
+    showMove('paper',compMove)
+    tellVictor('paper',compMove)
+})
+scissorMove.addEventListener('click',()=>{
+    const compMove = machineChoice()
+    showMove('scissor',compMove)
+    tellVictor('scissor',compMove)
+})
